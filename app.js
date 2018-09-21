ce97b78bf9254900f1b1987fef10b76173566fcb
Angel <a.morenoper@gmail.com>
2018-09-21 20:15:02 +0200

    App de prueba

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+  secret: 'mysecretpassword',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false,
+            maxAge:600000}
+}));
+var sess;
+app.get('/',function(req,res){
+
+  res.send("Request id: " + JSON.stringify(req.query));
+
+});
+app.post('/',function(req,res){
+
+  res.send("Request id: " + JSON.stringify(req.body));
+
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
